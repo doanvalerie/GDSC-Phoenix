9a2054aaa5886173593bb59ee9068eef5975a45a
Nivita Reddy <nivitareddy@campus-029-161.ucdavis.edu>
2023-03-01 00:29:47 -0800

    implemented folders

---
+++ lib/screens/folder_reader_screen.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import '../widgets/entry_display.dart';
+import 'note_reader_screen.dart';
+
+String folder_title = "";
+class FolderReaderScreen extends StatefulWidget {
+  FolderReaderScreen(this.doc, folder_name, {Key? key}) : super(key: key){folder_title=folder_name;}
+  final QueryDocumentSnapshot doc;
+
+  @override
+  State<FolderReaderScreen> createState() => _FolderReaderScreenState();
+}
+
+class _FolderReaderScreenState extends State<FolderReaderScreen> {
+  @override
+ Stream<QuerySnapshot<Object>> getStream() {
+    return  FirebaseFirestore.instance.collection("Folders").doc(folder_title).collection("Entries").snapshots();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Folder"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+                stream: getStream(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return ListView(
+                      padding: const EdgeInsets.all(8),
+                      children: snapshot.data!.docs
+                          .map<Widget>((note) => entryDisplay((){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                NoteReaderScreen(note),
+                          ),
+                        );
+                      }, note)).toList(),
+                    );
+                  }
+                  return const Text(
+                    "Create your first entry.",
+                  );
+                }
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
